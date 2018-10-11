08f977007e75ccc63c2bedf212c149808fff5129
Alessandro Canicatti <ales.canicatti@gmail.com>
2018-10-11 20:58:49 +0200

    request url is loaded by file. The $api_key placeholder is replaced by the real value

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import Data.Text.Template
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+computeRequest :: IO String
+computeRequest = do
+  body <- loadRequestBody
+  key <- loadAPIKey
+  return (mergeApiKey body key)
+
+loadRequestBody :: IO String
+loadRequestBody = readFile "resource/request_body"
+
+loadAPIKey :: IO String
+loadAPIKey = readFile "resource/api_key"
+
+mergeApiKey :: String -> String -> String
+mergeApiKey body key =
+  T.unpack $ L.toStrict $ substitute (T.pack body) (giveContext key)
+
+giveContext :: String -> Context
+giveContext apikey =
+  context [(T.pack "api_key", T.pack apikey)]
+
+-- | Create 'Context' from association list.
+context :: [(T.Text, T.Text)] -> Context
+context assocs x = maybe err id . lookup x $ assocs
+  where err = error $ "Could not find key: " ++ T.unpack x
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,22 @@
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception(evaluate)
+import Lib
 
 main :: IO ()
 main = hspec $ do
-  it "true should be true" $ do
-    True `shouldBe` True
+  it "should load request body from file" $ do
+    result <- loadRequestBody
+    result `shouldNotBe` ""
+
+  it "should load API key from file" $ do
+    result <- loadAPIKey
+    result `shouldNotBe` ""
+
+  it "should replace placeholder in string" $ do
+    mergeApiKey "this is a body with this $api_key placheholder" "key_to_substitute" `shouldContain` "key_to_substitute"
+
+  it "should use api key in request" $ do
+    result <- computeRequest
+    apikey <- loadAPIKey
+    result `shouldContain` apikey
